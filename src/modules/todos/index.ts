6e01ba938d946a5c245d92f78e84fe43330baa79
SeungHyun <fm10033@gmail.com>
2021-10-26 18:01:36 +0900

    모듈 파일 분리

--- src/modules/todos/index.ts
+++ src/modules/todos/index.ts
@@ -55,48 +55,6 @@
 // export default todos
 
 
-import { deprecated ,  ActionType, createReducer} from "typesafe-actions";
-
-const {createStandardAction} = deprecated
-const ADD_TODO = 'todos/ADD_TODO' as const
-const TOGGLE_TODO = 'todos/TOGGLE_TODO'  
-const REMOVE_TODO = 'todos/REMOVE_TODO' 
-
-let nextId = 1
- export const addTodo = (text:string)=> ({
-     type:ADD_TODO,
-     payload:{
-         id:nextId++,
-         text
-     }
- })
-
- export const toggleTodo = createStandardAction(TOGGLE_TODO)<number>()
- export const removeTodo = createStandardAction(REMOVE_TODO)<number>()
-
- const actions = {addTodo, toggleTodo, removeTodo}
- export type TodosAction = ActionType<typeof actions>
-
-export type Todo = {
-    id:number;
-    text:string;
-    done:boolean;
-}
-
-type TodosState = Todo[]
-const initialState: TodosState = []
-
-const todos = createReducer<TodosState, TodosAction>(initialState, { 
-    [ADD_TODO]: (state, action)=> state.concat({
-        ...action.payload,
-        done:false
-    }),
-    [TOGGLE_TODO]: (state, action)=> state.map(
-        todo=> todo.id === action.payload 
-        ? {...todo, done: !todo.done} 
-        : todo),
-    [REMOVE_TODO]: (state, action)=> state.filter(todo=> todo.id !== action.payload)
-
-})
-
-export default todos
+export {default} from './reducer' // reducer에서  내보낸 걸 불러와서 그대로 default로 내보내겠다
+export * from './actions' //actions에서 모든걸 불러와서 그대로 내보내주겠다
+export * from './types'

aaa96095ff502964f3ec02ba3ea0a2d71bb073a3
SeungHyun <fm10033@gmail.com>
2021-10-26 16:50:45 +0900

    typesafe-actions로 리덕스 모듈 리팩토링

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,50 +1,50 @@
-const INCREASE = 'counter/INCREASE' as const // ReturnType 사용하려고 붙임!
-const DECREASE = 'counter/DECREASE' as const
-const INCREASE_BY = 'counter/INCREASE_BY' as const
+import { ActionType , createReducer, deprecated} from "typesafe-actions"
 
-export const increase = ()=>({
-    type:INCREASE
-})
+const { createStandardAction} = deprecated;
 
-export const decrease = ()=>({
-    type:DECREASE
-})
+const INCREASE = 'counter/INCREASE' 
+const DECREASE = 'counter/DECREASE' 
+const INCREASE_BY = 'counter/INCREASE_BY' 
 
-export const increaseBy = (diff: number)=> ({
-    type:INCREASE_BY,
-    payload: diff
-})
+export const increase = createStandardAction(INCREASE)()  //윗줄과 같은 뜻! 
+export const decrease = createStandardAction(DECREASE)()
+export const increaseBy = createStandardAction(INCREASE_BY)<number>() 
 
 type CounterState = {
     count:number
 }
 
-type CounterAction = 
-| ReturnType<typeof increase> // returnType을 사용하면 함수 increase의 결과물을 타입으로 가져올 수 있음
-| ReturnType<typeof decrease>
-| ReturnType<typeof increaseBy>
+const actions = {increase, decrease, increaseBy}
+
+type CounterAction = ActionType<typeof actions> 
 
 const initialState : CounterState = {
     count: 0
 }
 
-function counter(state : CounterState = initialState, action: CounterAction) : CounterState{  //:CounterState ==> 리듀서 함수의 리턴 타입을 CounterState로 정함
-    switch(action.type){
-        case INCREASE:
-            return{
-                count: state.count + 1
-            }
-        case DECREASE:
-            return{
-                count: state.count - 1
-            }
-        case INCREASE_BY:
-            return{
-                count: state.count + action.payload
-            }
-        default:
-            return state
-    }
-}
+const counter = createReducer<CounterState, CounterAction>(initialState, { //createReducer로 reducer 함수 만들기
+    [INCREASE]: state => ({ count: state.count + 1}),
+    [DECREASE]: state => ({ count: state.count - 1}),
+    [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload})
+})
+
+// function counter(state : CounterState = initialState, action: CounterAction) : CounterState{  //:CounterState ==> 리듀서 함수의 리턴 타입을 CounterState로 정함
+//     switch(action.type){
+//         case INCREASE:
+//             return{
+//                 count: state.count + 1
+//             }
+//         case DECREASE:
+//             return{
+//                 count: state.count - 1
+//             }
+//         case INCREASE_BY:
+//             return{
+//                 count: state.count + action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 export default counter;
--- src/modules/todos.ts
+++ src/modules/todos.ts
@@ -1,30 +1,81 @@
+// const ADD_TODO = 'todos/ADD_TODO' as const  // ReturnType 사용하려고 붙임!
+// const TOGGLE_TODO = 'todos/TOGGLE_TODO'  as const
+// const REMOVE_TODO = 'todos/REMOVE_TODO' as const
+
+// let nextId = 1
+//  export const addTodo = (text:string) => ({
+//      type:ADD_TODO,
+//      payload: {
+//         id: nextId++,
+//         text
+//     }
+//  })
+
+//  export const toggleTodo = (id:number)=> ({
+//      type:TOGGLE_TODO,
+//      payload: id
+//  })
+
+//  export const removeTodo = (id:number)=> ({
+//      type: REMOVE_TODO,
+//      payload: id
+//  })
+
+// type TodoAction = 
+// |ReturnType<typeof addTodo> // returnType을 사용하면 함수 addTodo의 결과물을 타입으로 가져올 수 있음
+// |ReturnType<typeof toggleTodo>
+// |ReturnType<typeof removeTodo>
+
+// export type Todo = {
+//     id:number;
+//     text:string;
+//     done:boolean;
+// }
+
+// type TodosState = Todo[]
+// const initialState: TodosState = []
+
+// function todos(state:TodosState = initialState, action:TodoAction){
+//     switch(action.type){
+//         case ADD_TODO:
+//             return state.concat({
+//                 id:action.payload.id,
+//                 text: action.payload.text,
+//                 done:false
+//             })
+//         case TOGGLE_TODO:
+//             return state.map(todo=> todo.id === action.payload ? {...todo, done: !todo.done} : todo )
+//         case REMOVE_TODO:
+//             return state.filter(todo=> todo.id !== action.payload)
+//         default:
+//             return state
+//     }
+// }
+
+// export default todos
+
+
+import { deprecated ,  ActionType, createReducer} from "typesafe-actions";
+
+const {createStandardAction} = deprecated
 const ADD_TODO = 'todos/ADD_TODO' as const
-const TOGGLE_TODO = 'todos/TOGGLE_TODO'  as const
-const REMOVE_TODO = 'todos/REMOVE_TODO' as const
+const TOGGLE_TODO = 'todos/TOGGLE_TODO'  
+const REMOVE_TODO = 'todos/REMOVE_TODO' 
 
 let nextId = 1
- export const addTodo = (text:string) => ({
+ export const addTodo = (text:string)=> ({
      type:ADD_TODO,
-     payload: {
-        id: nextId++,
-        text
-    }
+     payload:{
+         id:nextId++,
+         text
+     }
  })
 
- export const toggleTodo = (id:number)=> ({
-     type:TOGGLE_TODO,
-     payload: id
- })
+ export const toggleTodo = createStandardAction(TOGGLE_TODO)<number>()
+ export const removeTodo = createStandardAction(REMOVE_TODO)<number>()
 
- export const removeTodo = (id:number)=> ({
-     type: REMOVE_TODO,
-     payload: id
- })
-
-type TodoAction = 
-|ReturnType<typeof addTodo>
-|ReturnType<typeof toggleTodo>
-|ReturnType<typeof removeTodo>
+ const actions = {addTodo, toggleTodo, removeTodo}
+ export type TodosAction = ActionType<typeof actions>
 
 export type Todo = {
     id:number;
@@ -35,21 +86,17 @@ export type Todo = {
 type TodosState = Todo[]
 const initialState: TodosState = []
 
-function todos(state:TodosState = initialState, action:TodoAction){
-    switch(action.type){
-        case ADD_TODO:
-            return state.concat({
-                id:action.payload.id,
-                text: action.payload.text,
-                done:false
-            })
-        case TOGGLE_TODO:
-            return state.map(todo=> todo.id === action.payload ? {...todo, done: !todo.done} : todo )
-        case REMOVE_TODO:
-            return state.filter(todo=> todo.id !== action.payload)
-        default:
-            return state
-    }
-}
+const todos = createReducer<TodosState, TodosAction>(initialState, { 
+    [ADD_TODO]: (state, action)=> state.concat({
+        ...action.payload,
+        done:false
+    }),
+    [TOGGLE_TODO]: (state, action)=> state.map(
+        todo=> todo.id === action.payload 
+        ? {...todo, done: !todo.done} 
+        : todo),
+    [REMOVE_TODO]: (state, action)=> state.filter(todo=> todo.id !== action.payload)
+
+})
 
 export default todos

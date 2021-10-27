2f4c401966ca4909a49033f53a1299264e7e64b9
SeungHyun <fm10033@gmail.com>
2021-10-27 18:52:53 +0900

    컨테이너 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CounterContainer from "./containers/CounterContainer";
+import GithubProfileLoader from "./containers/GithubProfileLoader";
 import TodoApp from "./containers/TodoApp";
 // import ReducerSample from './ReducerSample';
 // import { SampleProvider } from './SampleContext';
@@ -12,6 +13,7 @@ function App() {
       </SampleProvider> */}
       <CounterContainer></CounterContainer>
       <TodoApp></TodoApp>
+      <GithubProfileLoader></GithubProfileLoader>
     </>
   );
 }
